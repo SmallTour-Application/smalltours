ae82be8368753aa9753a13e9b85fa8a12d01f5c2
0uh1004 <0uh1004@gmail.com>
2023-05-02 23:05:49 +0900

    일부 테이블 PK 수정

--- src/main/java/com/lattels/smalltour/persistence/GuideEducationRepository.java
+++ src/main/java/com/lattels/smalltour/persistence/GuideEducationRepository.java
@@ -2,11 +2,10 @@ package com.lattels.smalltour.persistence;
 
 
 import com.lattels.smalltour.model.GuideEducation;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface GuideEducationRepository extends JpaRepository<GuideEducation, Integer> {
+public interface GuideEducationRepository extends JpaRepository<GuideEducation, GuideEducation.GuideEducationID> {
   
  
 }
