bf3bbbb1a738b3ae61968ef14231376d5d65c7ea
lsh <saunghunlee@naver.com>
2023-05-02 10:16:50 +0900

    Initial commit

---
+++ src/main/java/com/lattels/smalltour/SmalltourApplication.java
@@ -0,0 +1,13 @@
+package com.lattels.smalltour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmalltourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmalltourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lattels/smalltour/SmalltourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lattels.smalltour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmalltourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
