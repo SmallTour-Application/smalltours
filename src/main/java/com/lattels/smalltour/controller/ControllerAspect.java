6a2ba3e6bd80af160f8bc6f1c41a40032df79fee
S8U <0uh1004@gmail.com>
2023-05-09 11:40:24 +0900

    Controller Exception AOP로 분리

---
+++ src/main/java/com/lattels/smalltour/controller/ControllerAspect.java
@@ -0,0 +1,29 @@
+package com.lattels.smalltour.controller;
+
+import com.lattels.smalltour.dto.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ControllerAspect {
+
+    /**
+     * 컨트롤러 오류 발생 시 오류 메시지를 반환시킵니다.
+     */
+    @Around("within(com.lattels.smalltour.controller.*)")
+    public Object exception(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            return joinPoint.proceed();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(ResponseDTO.builder().error(e.getMessage()).build());
+        }
+    }
+
+}
