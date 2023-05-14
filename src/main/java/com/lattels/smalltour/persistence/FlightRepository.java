5d6b449aaf3c151a53688821a5371dbe7ca0d16f
daekyumyoo <daekyum.dev@gmail.com>
2023-05-14 22:33:48 +0900

    비행기 정보 관련 API

--- src/main/java/com/lattels/smalltour/persistence/FlightRepository.java
+++ src/main/java/com/lattels/smalltour/persistence/FlightRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
-  
+
+    Flight findById(int id);
  
 }
