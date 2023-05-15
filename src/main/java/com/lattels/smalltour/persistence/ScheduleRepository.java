20d4634ddd0d1a557d0841c1d02c9cfc8d80a8c1
daekyumyoo <daekyum.dev@gmail.com>
2023-05-15 23:36:26 +0900

    여행 일정 구현

--- src/main/java/com/lattels/smalltour/persistence/ScheduleRepository.java
+++ src/main/java/com/lattels/smalltour/persistence/ScheduleRepository.java
@@ -1,11 +1,18 @@
 package com.lattels.smalltour.persistence;
 
+import com.lattels.smalltour.model.Member;
 import com.lattels.smalltour.model.Schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
 
- 
+    Schedule findById(int id);
+
+    @Query(value = "SELECT s FROM Schedule s JOIN s.tours t WHERE s.id = :id AND t.guide = :member")
+    Schedule findByIdAndMember(@Param("id") int id, @Param("member") Member member);
+
 }
