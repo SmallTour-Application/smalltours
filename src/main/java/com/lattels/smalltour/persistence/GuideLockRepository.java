fa0468c31f33417ca753436b137f89f0d0dc7c4e
lsh <saunghunlee@naver.com>
2023-06-12 01:09:45 +0900

    guideScheduleService 수정완료

--- src/main/java/com/lattels/smalltour/persistence/GuideLockRepository.java
+++ src/main/java/com/lattels/smalltour/persistence/GuideLockRepository.java
@@ -21,7 +21,7 @@ public interface GuideLockRepository extends JpaRepository<GuideLock, Integer> {
     //해당 가이드 특정 기간 날짜 가져오기
     //다른 회원들은 특정 기간동안 해당 가이드 사용 불가하기 위한 목적
     @Query("SELECT gl FROM GuideLock gl WHERE gl.guide.id = :guideId AND gl.startDay >= :startDay AND gl.endDay <= :endDay")
-    GuideLock findGuideLockPeriod(
+    List<GuideLock> findGuideLockPeriod(
             @Param("guideId") int guideId,
             @Param("startDay") LocalDate startDay,
             @Param("endDay") LocalDate endDay
