fc4228bdd6a1ec3e3eff164e2a2b07d1c317264d
lshun <saunghunlee@naver.com>
2023-10-29 15:29:40 +0900

    년도,날짜 입력시 해당 지역별 로그인 횟수 출력

--- src/main/java/com/lattels/smalltour/persistence/LogMemberRepository.java
+++ src/main/java/com/lattels/smalltour/persistence/LogMemberRepository.java
@@ -3,8 +3,24 @@ package com.lattels.smalltour.persistence;
 
 import com.lattels.smalltour.model.LogMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface LogMemberRepository extends JpaRepository<LogMember, Integer> {
+
+
+    @Query(value = "SELECT lm.region, count(lm.region) FROM LogMember lm WHERE MONTH(lm.loginDateTime) =:month AND YEAR(lm.loginDateTime) =:year GROUP BY lm.region")
+    List<Object[]> findBySearchRegionDay(@Param("month") int month,@Param("year") int year);
+
+    @Query(value = "SELECT count(lm.region) FROM LogMember lm WHERE MONTH(lm.loginDateTime) =:month AND YEAR(lm.loginDateTime) =:year")
+    Integer findByCountRegion(@Param("month") int month,@Param("year") int year);
+
+
+
 
 }
